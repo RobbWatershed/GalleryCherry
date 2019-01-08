e13dc84232b0d6ad873489ae303dcad8a8b69e75
Robb <35880555+RobbWatershed@users.noreply.github.com>
2019-01-08 22:29:13 +0100

    Parse models in XNXX

--- app/src/main/java/me/devsaki/hentoid/parsers/content/XnxxContent.java
+++ app/src/main/java/me/devsaki/hentoid/parsers/content/XnxxContent.java
@@ -4,9 +4,8 @@ import org.jsoup.nodes.Element;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import me.devsaki.hentoid.database.domains.Attribute;
 import me.devsaki.hentoid.database.domains.Content;
 import me.devsaki.hentoid.database.domains.ImageFile;
 import me.devsaki.hentoid.enums.AttributeType;
@@ -15,19 +14,21 @@ import me.devsaki.hentoid.enums.StatusContent;
 import me.devsaki.hentoid.parsers.ParseHelper;
 import me.devsaki.hentoid.util.AttributeMap;
 import pl.droidsonroids.jspoon.annotation.Selector;
-import timber.log.Timber;
 
 public class XnxxContent {
+
+    private static String PORNSTARS_MARKER = "Pornstars : ";
+    private static String TAGS_MARKER = " - Tags : ";
 
     @Selector(value = "head meta[name='twitter:url']", attr = "content")
     private String galleryUrl;
     @Selector(value = "head title")
     private String title;
     @Selector(value = ".descriptionGalleryPage")
-    private List<Element> metadata;
+    private String rawMetadata;
     @Selector(value = ".descriptionGalleryPage a")
     private List<Element> tags;
-    @Selector(value = ".downloadLink", attr="href")
+    @Selector(value = ".downloadLink", attr = "href")
     private List<String> imageLinks;
 
 
@@ -36,18 +37,32 @@ public class XnxxContent {
 
         result.setSite(Site.XNXX);
 
-        result.setTitle(title.substring(0, title.lastIndexOf("gallery") - 1));
+        int galleryIndex = title.lastIndexOf("gallery");
+        if (galleryIndex > -1)
+            result.setTitle(title.substring(0, title.lastIndexOf("gallery") - 1));
+        else result.setTitle(title);
 
         AttributeMap attributes = new AttributeMap();
         result.setAttributes(attributes);
 
         // Models
-        // TODO
-        if (metadata != null) {
-            //ParseHelper.parseAttributes(attributes, AttributeType.TAG, tags, true);
+        if (rawMetadata != null && rawMetadata.contains(PORNSTARS_MARKER)) {
+            int tagsPosition = rawMetadata.indexOf(TAGS_MARKER);
+
+            String[] stars;
+            if (tagsPosition > -1)
+                stars = rawMetadata.substring(0, tagsPosition).replace(PORNSTARS_MARKER, "").split(",");
+            else
+                stars = rawMetadata.replace(PORNSTARS_MARKER, "").split(",");
+
+            for (String s : stars) {
+                attributes.add(new Attribute(AttributeType.MODEL, s.trim(), "/" + s.trim()));
+            }
         }
 
-        if (tags != null) {
+        if (tags != null)
+
+        {
             ParseHelper.parseAttributes(attributes, AttributeType.TAG, tags, true);
         }
 
@@ -55,10 +70,15 @@ public class XnxxContent {
         result.setImageFiles(images);
 
         int order = 1;
-        for(String s : imageLinks) {
+        for (
+                String s : imageLinks)
+
+        {
             images.add(new ImageFile(order++, s, StatusContent.SAVED));
         }
-        if (images.size() > 0) result.setCoverImageUrl(images.get(0).getUrl());
+        if (images.size() > 0) result.setCoverImageUrl(images.get(0).
+
+                getUrl());
         result.setQtyPages(images.size());
 
 
