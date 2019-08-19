78a94d8ae25fbb895455e318495450f9118c9743
Robb <zeugmaonline@hotmail.com>
2019-08-19 17:23:31 +0200

    Post-merge changes

--- app/src/main/java/me/devsaki/hentoid/HentoidApp.java
+++ app/src/main/java/me/devsaki/hentoid/HentoidApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 
