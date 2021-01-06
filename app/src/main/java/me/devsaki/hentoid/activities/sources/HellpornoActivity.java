8e7ed67afe56422f1651d83b2096e3db3c8c1690
Robb <zeugmaonline@hotmail.com>
2021-01-06 09:19:01 +0100

    Remove ad overlay

--- app/src/main/java/me/devsaki/hentoid/activities/sources/HellpornoActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/HellpornoActivity.java
@@ -6,6 +6,7 @@ public class HellpornoActivity extends BaseWebActivity {
 
     private static final String DOMAIN_FILTER = "hellporno.com";
     private static final String[] GALLERY_FILTER = {"/albums/....+"};
+    private static final String[] DIRTY_ELEMENTS = {".pignr"};
 
     Site getStartSite() {
         return Site.HELLPORNO;
@@ -18,6 +19,7 @@ public class HellpornoActivity extends BaseWebActivity {
 
     @Override
     protected CustomWebViewClient getWebClient() {
+        addDirtyElements(DIRTY_ELEMENTS);
         CustomWebViewClient client = new CustomWebViewClient(GALLERY_FILTER, this);
         client.restrictTo(DOMAIN_FILTER);
         return client;
