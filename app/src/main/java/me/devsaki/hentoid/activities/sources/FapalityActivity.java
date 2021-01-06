796e698066243cdc1274d114bb47c23ccd23767f
Robb <zeugmaonline@hotmail.com>
2021-01-06 18:45:58 +0100

    remove ad space

--- app/src/main/java/me/devsaki/hentoid/activities/sources/FapalityActivity.java
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/FapalityActivity.java
@@ -6,6 +6,7 @@ public class FapalityActivity extends BaseWebActivity {
 
     private static final String DOMAIN_FILTER = "fapality.com";
     private static final String[] GALLERY_FILTER = {"fapality.com/.*/[0-9]+/.*/$"};
+    private static final String[] DIRTY_ELEMENTS = {".had"};
 
     Site getStartSite() {
         return Site.FAPALITY;
@@ -19,6 +20,7 @@ public class FapalityActivity extends BaseWebActivity {
 
     @Override
     protected CustomWebViewClient getWebClient() {
+        addDirtyElements(DIRTY_ELEMENTS);
         CustomWebViewClient client = new CustomWebViewClient(GALLERY_FILTER, this);
         client.restrictTo(DOMAIN_FILTER);
         return client;
