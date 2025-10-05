3d04f5266c30e2f9e58feab364cae15c35700802
Robb <35880555+robbwatershed@users.noreply.github.com>
2025-10-05 15:58:15 +0200

    Improve gallery detection on Fapality

--- app/src/main/java/me/devsaki/hentoid/activities/sources/FapalityActivity.kt
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/FapalityActivity.kt
@@ -3,8 +3,8 @@ package me.devsaki.hentoid.activities.sources
 import me.devsaki.hentoid.enums.Site
 
 private const val DOMAIN_FILTER = "fapality.com"
-private val GALLERY_FILTER = arrayOf("fapality.com/.*/[0-9]+/.*/\$")
-private val DIRTY_ELEMENTS = arrayOf(".had\",\"a[rel^='nofollow noopener']")
+private val GALLERY_FILTER = arrayOf("fapality.com/[\\w_%\\-]+/[0-9]+/[\\w_%\\-]+/\$")
+private val DIRTY_ELEMENTS = arrayOf(".had", "a[rel^='nofollow noopener']")
 
 class FapalityActivity : BaseBrowserActivity() {
     override fun getStartSite(): Site {
