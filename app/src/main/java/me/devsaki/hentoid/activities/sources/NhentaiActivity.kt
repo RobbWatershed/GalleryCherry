a7c0bfbd389407a106913c21e0427fc83bcc038a
VioletKnight <violet@knig.ht>
2026-04-13 21:50:03 +0200

    NH : Fix mark books as read / merged to work with the new layout

--- app/src/main/java/me/devsaki/hentoid/activities/sources/NhentaiActivity.kt
+++ app/src/main/java/me/devsaki/hentoid/activities/sources/NhentaiActivity.kt
@@ -1,6 +1,7 @@
 package me.devsaki.hentoid.activities.sources
 
 import android.net.Uri
+import android.webkit.JavascriptInterface
 import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
@@ -54,6 +55,9 @@ class NhentaiActivity : BaseBrowserActivity() {
         // Init fetch handler here for convenience
         fetchResponseHandler = { url, body -> client.onData(url, body) }
 
+        client.setJsStartupScripts("nhentai.js")
+        webView.addJavascriptInterface(NhJsInterface(), "nhJsInterface")
+
         return client
     }
 
@@ -100,6 +104,16 @@ class NhentaiActivity : BaseBrowserActivity() {
             } catch (e: IOException) {
                 Timber.e(e)
             }
+        }
+    }
+
+    inner class NhJsInterface {
+        @JavascriptInterface
+        @Suppress("unused")
+        fun isMarkable(bookId: String): Int {
+            val downloadedBooks: List<String> = allSiteUrls
+            val mergedBooks: List<String> = allMergedBooksUrls
+            return if (downloadedBooks.contains(bookId)) 1 else if (mergedBooks.contains(bookId)) 2 else 0
         }
     }
 }
