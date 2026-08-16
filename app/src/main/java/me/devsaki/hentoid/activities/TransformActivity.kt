16938651d232669d0b6641320f4afde23191ba01
VioletKnight <violet@knig.ht>
2026-08-16 10:15:15 +0200

    Fix

--- app/src/main/java/me/devsaki/hentoid/activities/TransformActivity.kt
+++ app/src/main/java/me/devsaki/hentoid/activities/TransformActivity.kt
@@ -524,7 +524,9 @@ class TransformActivity : BaseActivity(), RangeDialogFragment.Parent {
                         else BitmapInfo(sourceBmp.rawData)
                     } else BitmapInfo(transformStill(context, sourceBmp.rawData, params, true))
                 } catch (e: Exception) {
-                    toast(R.string.transform_error)
+                    withContext(Dispatchers.Main) {
+                        toast(R.string.transform_error)
+                    }
                     createExceptionLogFile(e, context)
                     return@withContext null
                 }
