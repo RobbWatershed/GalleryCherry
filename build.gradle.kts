633496c4423d84316002d62586b8095070e84966
VioletKnight <violet@knig.ht>
2026-09-12 10:13:33 +0200

    Bump gradle, kotlin, ksp, kapt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("com.android.application") version ("9.2.1") apply false // wait for 9.4.0-alpha07+
+    id("com.android.application") version ("9.4.0") apply false
     id("com.google.gms.google-services") version ("4.4.1") apply false
     id("io.objectbox") version "5.4.2" apply false
     id("org.sonarqube") version "7.5.0.8588"
     id("com.google.firebase.crashlytics") version "3.0.8" apply false
     // Kept for ObjectBox while they still use kapt (see https://github.com/objectbox/objectbox-java/issues/1075)
-    id("com.android.legacy-kapt") version ("9.2.1") apply false
+    id("com.android.legacy-kapt") version ("9.4.0") apply false
     id("com.mikepenz.aboutlibraries.plugin") version "14.1.0" apply false
 }
 
