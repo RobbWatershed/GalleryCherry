6243a5c91613277e8aad9f0ce8cceb71d3dfd889
VioletKnight <violet@knig.ht>
2026-09-17 21:32:05 +0200

    Fix crash when parsing xPath query on MRM (...by downgrading JSoup)

--- app/src/main/java/me/devsaki/hentoid/parsers/JsoupX.kt
+++ app/src/main/java/me/devsaki/hentoid/parsers/JsoupX.kt
@@ -5,6 +5,6 @@ import org.jsoup.select.Elements
 
 // Unique select method supporting both XPath (if query starts by $x) and XSS (if not)
 fun Element.selectX(query: String): Elements {
-    return if (query.startsWith("\$x")) this.selectXpath(query.substring(2))
+    return if (query.startsWith($$"$x")) this.selectXpath(query.substring(2))
     else this.select(query)
 }
